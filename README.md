68f509c31e967b0a1a9aa85dbfbacd43dac5c968
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-02-05 14:09:50 +0100

    Add real argument parser

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # griefficient-filter
 
-Runs [<img src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667" alt="gh" width="18"/>](https://github.com/Odyssey346/Griefficient) **[Odyssey346/Griefficient](https://github.com/Odyssey346/Griefficient)** and filters it's output.
+Runs [<img src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667" alt="gh" width="18"/>](https://github.com/Odyssey346/Griefficient) **[Odyssey346/Griefficient](https://github.com/Odyssey346/Griefficient)** and filters it's output using Python 3.
 
 ### ⚠️ Warning, only tested on Windows 10! ⚠️
 
@@ -9,22 +9,23 @@ Runs [<img src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8
 ## Installation
 
 - Install [<img src="https://camo.githubusercontent.com/b079fe922f00c4b86f1b724fbc2e8141c468794ce8adbc9b7456e5e1ad09c622/68747470733a2f2f6564656e742e6769746875622e696f2f537570657254696e7949636f6e732f696d616765732f7376672f6769746875622e737667" alt="gh" width="18"/>](https://github.com/Odyssey346/Griefficient) **[Odyssey346/Griefficient](https://github.com/Odyssey346/Griefficient)**
-- Download [main.py](https://powernukkit.github.io/DownGit/#/home?url=https:%2F%2Fgithub.com%2FMP3Martin%2Fgriefficient-filter%2Fblob%2Fmain%2Fmain.py&directFile=1)
+- Clone this project and `cd` into it
+- Run `python3 -m pip install -r requirements.txt`
 
 ## Usage
 
-- Run `python3 main.py` + arguments
+- Run `python3 main.py -h` to get help
   
 ## Examples
 
-- `python3 main.py -w 1.16,1.17`
+- `python3 main.py -m w -f 1.16,1.17`
   - **Only shows** servers on version **1.16.\*** and **1.17.\***
 
-- `python3 main.py -b 1.19,1.18.2`
+- `python3 main.py -m b -f 1.19,1.18.2`
   - **Shows all servers except** servers on version **1.19.\*** and **1.18.2**
 
-- `python3 main.py -b 1.19.1 -grad`
+- `python3 main.py -m b -f 1.19.1 -g`
   - **Shows all servers except** servers on version **1.19.1** and shows the servers gradually (on `ENTER` key press)
   
-- `python3 main.py -b fkjhgvfnikhjdgndthj -grad`
+- `python3 main.py -g`
   - **Shows 100 servers** and shows them gradually (on `ENTER` key press)
--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # made by MP3Martin, licensed under MIT license
 # this is the worst code i have ever made
 # idk why i didn't parse the output and then reconstruct it again
+import argparse
 import subprocess
 import sys
 import os
 
+# ARGUMENTS PARSING WRAPPER
+parser = argparse.ArgumentParser(description="Run griefficient and filter it's output.")
+parser.add_argument("-m", "--mode", dest="mode", choices=["b", "w"], default="b", help="the filtering mode (b for blacklist / w for whitelist) (default: b)")
+parser.add_argument("-f", "--filter", dest="filter", default="🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅", help="the filter string(s) that filter the version info (separate with commas)", type=str)
+parser.add_argument("-g", "--grad", dest="grad", action="store_const", const=True, default=False, help="if the servers should be shown gradually (on ENTER key press)")
+
 # CONSTS
 COLOREND = "\x1b[0m"
-ARGS = sys.argv
+# ARGS = sys.argv
+ARGS = parser.parse_args()
 UPLINE = "\033[A"
 
 # FUNCTIONS
@@ -25,112 +33,100 @@ def substring_after(s, delim):
 def cls():
   print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-# - initial formating -
-try:
-  griefficient = subprocess.check_output(['griefficient'], shell=True)
-except:
-  raise Exception("Griefficinet is not installed, find tutorial at https://github.com/Odyssey346/Griefficient")
-
-out = griefficient
-out = f"{out}"
-out = out[2:]
-out = out[:-3]
-out = out.replace("\\n", "\n")
-
-if out.count("\n") < 3:
-  final = out
-else:
-  out = f"\x1b[0;34;40m{out}"
-  out = replaceOnLine(out, "by Odyssey346", "\x1b[0;32;40mby Odyssey346", 0)
-  out = out.replace("\n", "\x1b[0m\n", 1)
-  out = out.replace("\\xe2\\x9c\\x85", "\U00002705")
-  out = out.replace("\\xf0\\x9f\\x94\\x8e", "\U0001F50E")
-  out = out.replace("\\xf0\\x9f\\x91\\x89", "\x1b[0;36;40m" + "\U0001F449")
-  out = out.replace("Environment variables are set properly.", "\x1b[0;32;40m" + "Environment variables are set properly." + COLOREND)
-  out = out.replace("Getting data from Shodan's API, please wait...", "\x1b[0;32;40m" + "Getting data from Shodan's API, please wait..." + COLOREND)
-  out = out.replace("results shown out", COLOREND + "results shown out")
-  out = out.replace("shown out of", "shown out of" + "\x1b[0;36;40m")
-  out = out.replace("possible results", COLOREND + "possible results")
-
-  # - seperate outputs -
-  firstLines = '\n'.join(out.split('\n')[:3])
-  serverLines = '\n'.join(out.split('\n')[3:-1])
-  lastLines = '\n'.join(out.split('\n')[-1:])
-
-  # - format serverLines -
-  serverLines = serverLines.replace("\n", "\n" + "\x1b[0;34;40m")
-  serverLines = f"\x1b[0;34;40m{serverLines}{COLOREND}"
-  serverLines = serverLines.replace("(", "\x1b[0;36;40m" + "(")
-
+# MAIN
+def main():
+  # - initial formating -
   try:
-    mode = ARGS[1].lower()
+    griefficient = subprocess.check_output(['griefficient'], shell=True)
   except:
-    # raise Exception("Please enter mode (-w for whitelist and -b for blacklist)")
-    mode = "-b"
+    raise Exception("Griefficinet is not installed, find tutorial at https://github.com/Odyssey346/Griefficient")
 
-  if mode == "-w" or mode == "-b":
-    pass
+  out = griefficient
+  out = f"{out}"
+  out = out[2:]
+  out = out[:-3]
+  out = out.replace("\\n", "\n")
+
+  if out.count("\n") < 3:
+    final = out
   else:
-    raise Exception("Unknow mode: " + mode)
+    out = f"\x1b[0;34;40m{out}"
+    out = replaceOnLine(out, "by Odyssey346", "\x1b[0;32;40mby Odyssey346", 0)
+    out = out.replace("\n", "\x1b[0m\n", 1)
+    out = out.replace("\\xe2\\x9c\\x85", "\U00002705")
+    out = out.replace("\\xf0\\x9f\\x94\\x8e", "\U0001F50E")
+    out = out.replace("\\xf0\\x9f\\x91\\x89", "\x1b[0;36;40m" + "\U0001F449")
+    out = out.replace("Environment variables are set properly.", "\x1b[0;32;40m" + "Environment variables are set properly." + COLOREND)
+    out = out.replace("Getting data from Shodan's API, please wait...", "\x1b[0;32;40m" + "Getting data from Shodan's API, please wait..." + COLOREND)
+    out = out.replace("results shown out", COLOREND + "results shown out")
+    out = out.replace("shown out of", "shown out of" + "\x1b[0;36;40m")
+    out = out.replace("possible results", COLOREND + "possible results")
 
-  try:
-    ignored = ARGS[2]
-  except:
-    ignored = "🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅🕅"
-  ignored = ignored.split(",")
+    # - seperate outputs -
+    firstLines = '\n'.join(out.split('\n')[:3])
+    serverLines = '\n'.join(out.split('\n')[3:-1])
+    lastLines = '\n'.join(out.split('\n')[-1:])
 
-  try:
-    grad = ARGS[3]
-    if grad == "-grad":
-      grad = True
+    # - format serverLines -
+    serverLines = serverLines.replace("\n", "\n" + "\x1b[0;34;40m")
+    serverLines = f"\x1b[0;34;40m{serverLines}{COLOREND}"
+    serverLines = serverLines.replace("(", "\x1b[0;36;40m" + "(")
+
+    # - parse ARGS -
+    mode = ARGS.mode
+
+    ignored = ARGS.filter
+    ignored = ignored.split(",")
+
+    grad = ARGS.grad
+
+    # - filter serverLines -
+    serverLines = serverLines.split("\n")
+    tmpServerLines = []
+    if mode == "w":
+      # whitelist mode
+      for line in serverLines:
+        if any(bad in substring_after(line, "(") for bad in ignored):
+          tmpServerLines.append(line)
     else:
-      grad = False
-  except:
-    grad = False
+      # blacklist mode
+      for line in serverLines:
+        if not any(bad in substring_after(line, "(") for bad in ignored):
+          tmpServerLines.append(line)
+    serverLines = "\n".join(tmpServerLines)
 
-  serverLines = serverLines.split("\n")
-  tmpServerLines = []
-  if mode == "-w":
-    # whitelist mode
-    for line in serverLines:
-      if any(bad in substring_after(line, "(") for bad in ignored):
-        tmpServerLines.append(line)
+    # - additional formating -
+    if str.encode(serverLines) == b'':
+      resultCount = 0
+    else:
+      resultCount = serverLines.count('\n') + 1
+    
+    lastLines = lastLines.replace("100", str(resultCount))
+
+    # - combine all *Lines -
+    final = f"{firstLines}\n{serverLines}\n{lastLines}"
+
+  grad_i = 0
+
+  if resultCount == 0:
+    print("No results found!")
+    exit()
+
+  if not grad:
+    print(final)
   else:
-    # blacklist mode
-    for line in serverLines:
-      if not any(bad in substring_after(line, "(") for bad in ignored):
-        tmpServerLines.append(line)
-  serverLines = "\n".join(tmpServerLines)
+    print("\n\n\n\n\n\n\n\n")
+    for line in serverLines.split("\n"):
+      grad_i = grad_i + 1
+      addSpaces = ""
+      for _ in range(int(str(len(line) - os.get_terminal_size().columns).replace("-", "")) + 20):
+        addSpaces = addSpaces + " "
+      a = firstLines
+      b = "\n\n" + line + addSpaces + "\n\n"
+      c = f"\x1b[0;36;40m👉 \x1b[0;33;40m#{grad_i} \x1b[0mof \x1b[0;36;40m{str(resultCount)} \x1b[0mresults shown out of\x1b[0;36;40m 215187 \x1b[0mpossible results (blame Shodan)"
+      print(UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+a+b+c)
+      if input(f"Press \x1b[0;33;40mENTER{COLOREND} to show \x1b[0;33;40mnext server{COLOREND}. Type \x1b[0;33;40mEND{COLOREND} and press \x1b[0;33;40mENTER{COLOREND} to \x1b[0;33;40mend the program{COLOREND}.").lower() == "end":
+        exit()
 
-  # - additional formating -
-  if str.encode(serverLines) == b'':
-    resultCount = 0
-  else:
-    resultCount = serverLines.count('\n') + 1
-  
-  lastLines = lastLines.replace("100", str(resultCount))
 
-  # - combine all *Lines -
-  final = f"{firstLines}\n{serverLines}\n{lastLines}"
-
-grad_i = 0
-
-if resultCount == 0:
-  print("No results found!")
-  exit()
-
-if not grad:
-  print(final)
-else:
-  print("\n\n\n\n\n\n\n\n")
-  for line in serverLines.split("\n"):
-    grad_i = grad_i + 1
-    addSpaces = ""
-    for _ in range(int(str(len(line) - os.get_terminal_size().columns).replace("-", "")) + 20):
-      addSpaces = addSpaces + " "
-    a = firstLines
-    b = "\n\n" + line + addSpaces + "\n\n"
-    c = f"\x1b[0;36;40m👉 \x1b[0;33;40m#{grad_i} \x1b[0mof \x1b[0;36;40m{str(resultCount)} \x1b[0mresults shown out of\x1b[0;36;40m 215187 \x1b[0mpossible results (blame Shodan)"
-    print(UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+UPLINE+a+b+c)
-    if input(f"Press \x1b[0;33;40mENTER{COLOREND} to show \x1b[0;33;40mnext server{COLOREND}. Type \x1b[0;33;40mEND{COLOREND} and press \x1b[0;33;40mENTER{COLOREND} to \x1b[0;33;40mend the program{COLOREND}.").lower() == "end":
-      exit()
+main()
